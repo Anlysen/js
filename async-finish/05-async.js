518a0c1dc97f9ec2dcedc177dfc2798c8c44952b
Andrew <rostangeles1993@gmail.com>
2024-01-12 17:28:42 +0300

    Async

--- async-finish/05-async.js
+++ async-finish/05-async.js
@@ -1,58 +1,55 @@
-// Event Loop
+// Асинхронность
+// Event loop
 
 // const timeout = setTimeout(() => {
 //   console.log('after 2 seconds')
 // }, 2000)
 
-// clearTimeout(timeout)
+// clearTimeout(timeout) // Удаляет этот цикл
 
 // setTimeout(() => {
 //   console.log('after 3 seconds')
 // }, 3000)
+
 // let count = 0
 // setInterval(() => {
-//   console.log('tick', ++count)
+//   console.log('Tick', ++count)
 // }, 1000)
 
-// function delay(callback, time = 1000) {
-//   setTimeout(callback, time)
+// function delay (callBack, time = 1000) {
+//   setTimeout(callBack, time)
 // }
 
+// delay(() => {
+//   console.log('timeout')
+// }, 2000)
+
+
+// ===========
+
+
+// Promise
 const delay = (time = 1000) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve([1, 2, 3])
+         resolve([1, 2, 3])
       // reject('Error in delay')
     }, time)
   })
   return promise
 }
+// resolve, reject - предупреждает, что какой-то асинхронный код завершился
 
-// delay(2500)
-//   .then((data) => {
-//     console.log('Timeout', data)
-//     return data.map((x) => x ** 2)
-//   })
-//   .then((data) => {
-//     console.log(data)
-//   })
-//   .catch((err) => {
-//     console.log(err)
-//   })
-//   .finally(() => console.log('Finally'))
+// delay(2500).then((data) => {
+//   console.log('Timeout', data)
+//   return data.map((x) => x ** 2)
+// })
+// .then((data) => {
+//   console.log(data)
+// })
+// .catch((err) => {
+//   console.log(err) // Отлов ошибок
+// })
+// .finally(() => console.log('Finally'))
 
-const getData = () => new Promise((resolve) => resolve([1, 2, 3]))
 
-async function asyncExample() {
-  try {
-    await delay(3000)
-    const data = await getData()
-    console.log(data)
-  } catch (err) {
-    console.log(err)
-  } finally {
-    console.log('Finally')
-  }
-}
-
-asyncExample()
